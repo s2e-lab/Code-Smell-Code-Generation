e9861b20fa3ead5322cc9f2ae552ac49fbf652f9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 18:39:50 +0000

    Adding numeric_owner as keyword arguement

--- Validation/BanditSamples/B602_Clippy_Parsed_Functions_1_data_2230_time1626325908_default.jsonl_702.py
+++ Validation/BanditSamples/B602_Clippy_Parsed_Functions_1_data_2230_time1626325908_default.jsonl_702.py
@@ -84,7 +84,7 @@ class Trial:
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar, trial_code_dir)
